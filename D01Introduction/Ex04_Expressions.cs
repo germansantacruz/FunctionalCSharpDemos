c7383dea13c9a1dcdd7064ac6da796bc5a2b6465
Germán Santa Cruz <german.scf@hotmail.com>
2022-05-12 16:03:38 -0400

    update D01

---
+++ D01Introduction/Ex04_Expressions.cs
@@ -0,0 +1,52 @@
+﻿namespace D01Introduction;
+
+public static class Ex04_Expressions
+{
+    private class MyClassicSingleton
+    {
+        private static MyClassicSingleton instance;
+
+        private MyClassicSingleton() { }
+
+        public static MyClassicSingleton Instance
+        {
+            get
+            {
+                if (instance is null)
+                {
+                    instance = new MyClassicSingleton();
+                }
+
+                return instance;
+            }
+        }
+        // Other stuff here
+    }
+
+    // Las expresiones se ejecutan por su resultado.
+    private class MySingleton
+    {
+        private static MySingleton instance;
+
+        private MySingleton() { }
+
+        public static MySingleton Instance
+            => instance ??= new MySingleton();
+
+        public override string ToString()
+            => $"Type name: {GetType().Name.Split('+')[0]}";
+    }
+
+    public static void RunExample(ConsoleColor color)
+    {
+        Util.ShowTitle("Preferir expresiones sobre declaraciones:", color);
+
+        var instance1 = MyClassicSingleton.Instance;
+        var instance2 = MyClassicSingleton.Instance;
+        Console.WriteLine($"Same Object? {ReferenceEquals(instance1, instance2)}");
+
+        var instance3 = MySingleton.Instance;
+        var instance4 = MySingleton.Instance;
+        Console.WriteLine($"Same Object? {ReferenceEquals(instance3, instance4)}");
+    }
+}
--- D01Introduction/Util.cs
+++ D01Introduction/Util.cs
@@ -16,9 +16,10 @@ public static class Util
         Console.WriteLine("****************************************************************");
         Console.WriteLine("                              Menú");
         Console.WriteLine("****************************************************************");
-        Console.WriteLine("1. Imperativa vs Funcional     2. Inmutabilidad    3. Concurrencia");
-        Console.WriteLine("4. Expression Bodied Members   5. LINQ             6. Funciones locales");
-        Console.WriteLine("7. Init only setters           8. Tuplas           9. Pattern Matching");
+        Console.WriteLine("1. Imperativa vs Funcional     2. Inmutabilidad      3. Concurrencia");
+        Console.WriteLine("4. Preferir expresiones        5. Init only setters  6. LINQ");
+        Console.WriteLine("7. Expression Bodied Members   8. Funciones locales  9. Tuplas");
+        Console.WriteLine("10. Pattern Matching");
         Console.WriteLine("Elija una opción.");
     }
 
@@ -43,21 +44,24 @@ public static class Util
                     Ex03_Concurrency.RunExample(color);
                     break;
                 case "4":
-                    F02_ExpressionBodiedMembers.RunExample(color);
+                    Ex04_Expressions.RunExample(color);
                     break;
                 case "5":
-                    F01_LINQ.RunExample(color);
-                    break;
-                case "6":
-                    F03_FunctionsWithinFunctions.RunExample(color);
-                    break;
-                case "7":
                     F05_InitOnlySetters.RunExample(color);
                     break;
+                case "6":
+                    F01_LINQ.RunExample(color);
+                    break;
+                case "7":
+                    F02_ExpressionBodiedMembers.RunExample(color);
+                    break;
                 case "8":
-                    F04_Tuples.RunExample(color);
+                    F03_FunctionsWithinFunctions.RunExample(color);
                     break;
                 case "9":
+                    F04_Tuples.RunExample(color);
+                    break;
+                case "10":
                     F06_PatternMatching.RunExample(color);
                     break;
                 case "clear":
